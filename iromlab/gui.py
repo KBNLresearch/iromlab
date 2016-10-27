a4fea3d3e18abb4facda6437dc12060f8539fb71
Johan van der Knijff <johan.vanderknijff@kb.nl>
2016-10-27 18:04:56 +0200

    Inproved GUI script, now works on both Py2 and Py3

---
+++ iromlab/gui.py
@@ -0,0 +1,105 @@
+"""
+Adapted from: http://www.datadependence.com/2016/04/how-to-build-gui-in-python-3/
+"""
+ 
+try:
+    import tkinter as tk #Python 3.x
+    from tkinter import ttk as ttk
+except ImportError:
+    import Tkinter as tk # Python 2.x
+    import ttk as ttk
+     
+class carrierEntry(tk.Frame):
+    
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.root = parent
+        self.init_gui()
+ 
+    def on_quit(self):
+        # Exits program.
+        quit()
+ 
+    def submit(self):
+        # Fetch entered values
+        catid = self.catid_entry.get()
+        volumeNo = self.volumeNo_entry.get()
+        noVolumes = self.noVolumes_entry.get()
+        carrierType = self.v.get()
+        
+        self.answer_label['text'] = catid
+        
+        print(catid,volumeNo,noVolumes)
+        print(self.v.get())
+        
+        # Reset entry fields
+        self.catid_entry.delete(0, tk.END)
+        self.volumeNo_entry.delete(0, tk.END)
+        self.noVolumes_entry.delete(0, tk.END)
+  
+    def init_gui(self):
+        # Build GUI
+        self.root.title('iromlab')
+        self.root.option_add('*tearOff', 'FALSE')
+ 
+        self.grid(column=0, row=0, sticky='nsew')
+ 
+        tk.Label(self, text='Enter carrier details').grid(column=0, row=0,
+                columnspan=4)
+       
+        ttk.Separator(self, orient='horizontal').grid(column=0,
+                row=1, columnspan=4, sticky='ew')
+                
+        # Catalog ID        
+        tk.Label(self, text='Catalog ID (PPN)').grid(column=0, row=2,
+                sticky='w')
+        self.catid_entry = tk.Entry(self, width=10)
+        self.catid_entry.grid(column=2, row = 2)
+
+        # Volume number
+        tk.Label(self, text='Volume number').grid(column=0, row=3,
+                sticky='w')
+        self.volumeNo_entry = tk.Entry(self, width=5)
+        self.volumeNo_entry.grid(column=2, row=3)
+
+        # Number of volumes
+        tk.Label(self, text='Number of volumes').grid(column=0, row=4,
+                sticky='w')
+        self.noVolumes_entry = tk.Entry(self, width=5)
+        self.noVolumes_entry.grid(column=2, row=4)
+ 
+        # Carrier type (radio button select)
+        self.v = tk.IntVar()
+        self.v.set(1)
+        
+        tk.Label(self, text='Carrier type:').grid(column=0, row=5,
+                sticky='w', columnspan=4)
+                
+        self.rb1 = tk.Radiobutton(self, text='cd-rom', variable=self.v, value=1).grid(column=0, row=6, 
+            sticky='w')
+        self.rb2 = tk.Radiobutton(self, text='cd-audio', variable=self.v, value=2).grid(column=0, row=7, 
+            sticky='w')
+        self.rb3 = tk.Radiobutton(self, text='dvd-rom', variable=self.v, value=3).grid(column=0, row=8, 
+            sticky='w')
+        self.rb4 = tk.Radiobutton(self, text='dvd-video', variable=self.v, value=4).grid(column=0, row=9, 
+            sticky='w')   
+  
+        self.submit_button = tk.Button(self, text='Submit',
+                command=self.submit)
+        self.submit_button.grid(column=0, row=10, columnspan=4)
+ 
+        self.answer_frame = tk.LabelFrame(self, text='Answer',
+                height=100)
+        self.answer_frame.grid(column=0, row=11, columnspan=4, sticky='nesw')
+ 
+        self.answer_label = tk.Label(self.answer_frame, text='')
+        self.answer_label.grid(column=0, row=0)
+  
+        for child in self.winfo_children():
+            child.grid_configure(padx=5, pady=5)
+ 
+if __name__ == '__main__':
+    root = tk.Tk()
+    carrierEntry(root)
+    root.mainloop()
+
