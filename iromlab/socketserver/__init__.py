d59bd1da1d8a1b26b764f9a6a83a34079e2b6b21
Johan van der Knijff <johan.vanderknijff@kb.nl>
2019-05-08 17:32:37 +0200

    server code now runs

--- iromlab/socketserver/__init__.py
+++ iromlab/socketserver/__init__.py
@@ -1,7 +1,1 @@
-from .server import start_server
-__version__ = '0.1.5'
-
-__all__ = ['sru']
-__author__ = 'WillemJan Faber <willemjan.faber@kb.nl'
-
-sru = sru()
+from .server import server
--- iromlab/socketserver/server.py
+++ iromlab/socketserver/server.py
@@ -7,44 +7,52 @@ import traceback
 
 from . import libserver
 
-sel = selectors.DefaultSelector()
+class server():
 
+    sel = selectors.DefaultSelector()
 
-def accept_wrapper(sock):
-    conn, addr = sock.accept()  # Should be ready to read
-    print("accepted connection from", addr)
-    conn.setblocking(False)
-    message = libserver.Message(sel, conn, addr)
-    sel.register(conn, selectors.EVENT_READ, data=message)
+    def accept_wrapper(self, sock):
+        conn, addr = sock.accept()  # Should be ready to read
+        print("accepted connection from", addr)
+        conn.setblocking(False)
+        message = libserver.Message(self.sel, conn, addr)
+        self.sel.register(conn, selectors.EVENT_READ, data=message)
 
-def start_server(host, port):
+    def start_server(self, host, port):
 
-    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Avoid bind() exception: OSError: [Errno 48] Address already in use
-    lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    lsock.bind((host, port))
-    lsock.listen()
-    print("listening on", (host, port))
-    lsock.setblocking(False)
-    sel.register(lsock, selectors.EVENT_READ, data=None)
+        lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Avoid bind() exception: OSError: [Errno 48] Address already in use
+        lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        lsock.bind((host, port))
+        lsock.listen()
+        print("listening on", (host, port))
+        lsock.setblocking(False)
+        self.sel.register(lsock, selectors.EVENT_READ, data=None)
 
-    try:
-        while True:
-            events = sel.select(timeout=None)
-            for key, mask in events:
-                if key.data is None:
-                    accept_wrapper(key.fileobj)
-                else:
-                    message = key.data
-                    try:
-                        message.process_events(mask)
-                    except Exception:
-                        print(
-                            "main: error: exception for",
-                            f"{message.addr}:\n{traceback.format_exc()}",
-                        )
-                        message.close()
-    except KeyboardInterrupt:
-        print("caught keyboard interrupt, exiting")
-    finally:
-        sel.close()
+        try:
+            while True:
+                events = self.sel.select(timeout=None)
+                for key, mask in events:
+                    if key.data is None:
+                        self.accept_wrapper(key.fileobj)
+                    else:
+                        message = key.data
+                        try:
+                            message.process_events(mask)
+                        except Exception:
+                            print(
+                                "main: error: exception for",
+                                f"{message.addr}:\n{traceback.format_exc()}",
+                            )
+                            message.close()
+        except KeyboardInterrupt:
+            print("caught keyboard interrupt, exiting")
+        finally:
+            self.sel.close()
+
+def main():
+    host = '127.0.0.1'
+    port = 65432
+    myServer = server()
+    myServer.start_server(host, port)
+main()
