0c0fc041633b76719fcc0149e7c8172528b153f0
Johan van der Knijff <johan.vanderknijff@kb.nl>
2019-05-21 14:35:19 +0200

    removed server launcher, updated version string

--- iromlab/iromlab.pyw
+++ iromlab/iromlab.pyw
@@ -38,7 +38,7 @@ from . import cdworker
 from . import cdinfo
 
 
-__version__ = '0.11.0'
+__version__ = '1.0.0b1'
 config.version = __version__
 
 class carrierEntry(tk.Frame):
--- server-launch.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-"""Wrapper script, ensures that relative imports work correctly in a PyInstaller build"""
-
-from iromlab.socketserver.server import main
-
-if __name__ == '__main__':
-    main()
