9874c19a962dd65156ef2b31fee1eb9d11896607
bitsgalore <johan.vanderknijff@kb.nl>
2020-08-24 14:04:03 +0200

    call twine as a Python module to avoid command not found error

--- package-pypi.sh
+++ package-pypi.sh
@@ -28,7 +28,7 @@ python3 setup.py sdist bdist_wheel --universal
 
 # Upload package if wheel build was successful; if not show error message
 if [ $? -eq 0 ]; then
-    twine upload --repository $repository  dist/*
+    python3 -m twine upload --repository $repository  dist/*
 else
     echo "Wheel build not successful quitting now ..."
 fi
